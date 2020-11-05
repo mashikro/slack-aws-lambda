55b01aac5471c6b10ce394674cc4bb9e04625a4c
Masha Ikromova <mashikro@gmail.com>
2020-11-05 16:06:59 -0500

    removed comments and added func docstrings

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# Create a lambda function
-
-# Add this code to your lambda function code editor ######
+# Add this code to your lambda function code editor
 
 from urllib import request, parse
 import json
@@ -9,7 +7,8 @@ import random
 
 
 def send_slack_message(webhook, text):
-    compliment = "Hello! You are {text}".format(text=text)
+    '''Sends a slack message using python's urllib'''
+    compliment = "Hello! You are {text}!".format(text=text)
     raw_data = {
         'text': compliment,
         'username': 'mash',
@@ -23,10 +22,12 @@ def send_slack_message(webhook, text):
 
     
 def generate_random_adjective():
+    '''Generates a random adjective'''
     adjectives = ['clever', 'intelligent', 'smart', 'funny', 'cold', 'sour', 'scrawny', 'fluffy', 'pretty']
     return random.choice(adjectives)
 
 def lambda_handler(event, context):
+    '''Lambda handler. Uses helper functions to do the job. Required!'''
 
     text = generate_random_adjective() #using AWS Lambda enviroment var
     webhook = os.environ["slack_webhook"]
@@ -36,9 +37,3 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Messege sent!')
     }
-
-##### Setting up a schedule to call your lambda function #####
-
-# Follow this tutuorial: 
-# https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/RunLambdaSchedule.html
-
